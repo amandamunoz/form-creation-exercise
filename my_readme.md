9c0356da09f7d3fff915de8e6e9ea0d486c7abe1
Amanda Munoz <amanda@ideas42ventures.com>
2023-06-26 14:30:54 -0400

    Add ticket totaling functionality

---
+++ my_readme.md
@@ -0,0 +1,27 @@
+Notes
+
+Header:
+name
+date
+location
+
+left column:
+imgUrl
+description_blurb
+
+right column:
+ticket selection
+total
+name info
+payment info
+button
+
+Strategy:
+Get all of the base components working
+
+To do:
+Format date
+Make image smaller
+Safely render html
+Revisit html/css structure
+Validation for the number input (positive numbers only)
--- src/BandForm.js
+++ src/BandForm.js
@@ -1,12 +1,45 @@
+import { useState } from "react";
+
 function BandForm({ band }) {
+  const [totalTicketCosts, setTotalTicketCosts] = useState({})
+  const [totalTicketCost, setTotalTicketCost] = useState(0);
+
+  function addToTotal(event, ticket) {
+    const amount = event.target.value * ticket.cost/100;
+    const newTotalCosts = {...totalTicketCosts, [ticket.type]: amount}
+    const newTotalValues = Object.values(newTotalCosts)
+    const totalCost  = newTotalValues.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue}, 0 )
+    setTotalTicketCosts(newTotalCosts)
+    setTotalTicketCost(totalCost);
+  }
+
   return (
     <div>
       <h1>{band.name}</h1>
-      {band.ticketTypes.map((ticket) => (
-        <p>
-          {ticket.name} - {ticket.description}
-        </p>
-      ))}
+      <p>{String(new Date(band.date * 1000))}</p>
+      <p>{band.location}</p>
+      <div className="container">
+        <div>
+          <img src={band.imgUrl} alt={band.name}></img>
+          <div dangerouslySetInnerHTML={{__html: band.description_blurb}} />
+        </div>
+        <div>
+          <h2>Select Tickets</h2>
+          {band.ticketTypes.map((ticket) => (
+            <div key={ticket.type}>
+              <p>{ticket.name}</p>
+              <p>{ticket.description}</p>
+              <p>{ticket.cost/100}</p>
+              <input type="number" id={`select_${ticket.type}`} onChange={event => addToTotal(event, ticket)}></input>
+            </div>
+          ))}
+          <div>
+            <p>Total</p>
+            <p>{totalTicketCost}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
